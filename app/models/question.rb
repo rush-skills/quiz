c2d03503aba18cea19060c4a5a2a61060eb4e069
Ankur Singh <ankur13019@iiitd.ac.in>
2015-03-05 16:39:16 +0000

    added cocoon for question answer creation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :test
   has_many :answers
+  accepts_nested_attributes_for :answers, :reject_if => :all_blank, :allow_destroy => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304183506) do
+ActiveRecord::Schema.define(version: 20150304185425) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "text"
+    t.boolean  "correct"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "test_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "questions", ["test_id"], name: "index_questions_on_test_id"
+
+  create_table "tests", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "free"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
